a7eedfcee23a69fa29e276e17a2436f584158c21
lucaslopescunha <lucaslopescunha@gmail.com>
2026-02-25 15:34:11 -0300

    Pipes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
+import { DatePipe} from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TemperaturePipe } from './temperature.pipe';
+import { SortPipe } from './sort-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [DatePipe, /**DecimalPipe,*/ TemperaturePipe, SortPipe]
 })
 export class App {
   protected readonly title = signal('weather-pipes');
+  currentDate = new Date();
+  currentTemperatures = {
+    berlin: 4.2749812,
+    newYork: 18.1214,
+    paris: 72.1209001,
+    chicago: 65.0775238
+  };
+  historicTemperatures = [
+    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5
+  ];
+
+  onReset(index: number) {
+    this.historicTemperatures[index] = 18;
+  }
 }
